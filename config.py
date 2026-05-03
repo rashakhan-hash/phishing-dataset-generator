2ed75850f21c865d012019d8733f8bf7cf46f755
rashakhan-hash <rashakhan918@gmail.com>
2026-05-03 15:52:42 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Phishing Dataset Generator Config
+admin_email = "rasha.khan.sec@gmail.com"
+api_key = "demo_key_12345"
+
+def generate_dataset():
+    print("Generating phishing dataset...")
+
+if __name__ == "__main__":
+    generate_dataset()
